94ab125d499519e8056a0181b4622fd376dc8ac0
dialoghiradio <teisasa3@gmail.com>
2026-07-11 12:55:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,41 @@ fetch(feed + "?nocache=" + new Date().getTime())
     const xml = parser.parseFromString(str, "text/xml");
 
     const items = [...xml.querySelectorAll("item")];
+    items.sort((a, b) => {
+    const dataA = new Date(a.querySelector("pubDate").textContent);
+    const dataB = new Date(b.querySelector("pubDate").textContent);
+    return dataB - dataA;
+});
+
+    // ===== ULTIMA PUNTATA =====
+const ultima = items[0];
+
+const titoloUltima = ultima.querySelector("title").textContent;
+const linkUltima = ultima.querySelector("link").textContent;
+const idUltima = linkUltima.match(/--(\d+)$/)[1];
+
+const evidenza = document.getElementById("ultima-puntata");
+
+evidenza.innerHTML = `
+    <h2>🎙 Ultima puntata</h2>
+    <h3>${titoloUltima}</h3>
+
+    <iframe
+    src="https://widget.spreaker.com/player?episode_id=${idUltima}&theme=light"
+    width="100%"
+    height="200"
+    frameborder="0"
+    allow="autoplay">
+    </iframe>
+
+    <hr>
+`;
 
     // ===== ULTIME PUNTATE =====
 
     const lista = document.getElementById("episodi");
 
-    items.slice(0,10).forEach(ep => {
+    items.slice(1,11).forEach(ep => {
 
         const titolo = ep.querySelector("title").textContent;
         const link = ep.querySelector("link").textContent;
